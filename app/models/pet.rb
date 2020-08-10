6cff3b0d78e28fa23e9420c1c5dd8c022963b9b7
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-08-10 16:22:43 -0400

    Add species id to pets

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,4 @@
 class Pet < ApplicationRecord
   belongs_to :user
-  has_one :species 
+  belongs_to :species 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_182918) do
+ActiveRecord::Schema.define(version: 2020_08_10_201629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_08_10_182918) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "species_id"
+    t.index ["species_id"], name: "index_pets_on_species_id"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
