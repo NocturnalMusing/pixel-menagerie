2f705f4ed80a59f7f6e11bd680192b1081c6a2e5
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-08-11 17:08:10 -0400

    Seed data again

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Species.destroy_all
-Pet.destroy_all
-User.destroy_all
+# Species.destroy_all
+# Pet.destroy_all
+# User.destroy_all
 
-@admin = User.create!(username: 'admin', email: 'admin@email.com', password: '123456')
-puts '#{User.count} users created'
+# @admin = User.create!(username: 'admin', email: 'admin@email.com', password: '123456')
+# puts '#{User.count} users created'
+
+# @mammals = Species.create!(name: 'Mammals')
+# @avians = Species.create!(name: 'Avians')
+# @aquatic = Species.create!(name: 'Aquatic')
+# @insect = Species.create!(name: 'Insect')
+# @mythic = Species.create!(name: 'Mythic')
+# @misc = Species.create!(name: '???')
+# puts '#{Species.count} species created'
 
 @crow = Pet.create!(
     name: 'Crow', 
     user: @admin,
     image: '',
-    about: 'Testing'
+    about: 'Testing',
+    species_id: 20,
+    user_id: 
 )
-
-@mammals = Species.create!(name: 'Mammals')
-@avians = Species.create!(name: 'Avians')
-@aquatic = Species.create!(name: 'Aquatic')
-@insect = Species.create!(name: 'Insect')
-@mythic = Species.create!(name: 'Mythic')
-@misc = Species.create!(name: '???')
