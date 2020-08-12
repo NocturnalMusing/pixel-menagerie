0b49c9f8056b45c26c9de5317076413eaf05691c
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-08-12 14:22:49 -0400

    Create reusable header component

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,14 @@ import React from 'react'
 import { Link, Route } from 'react-router-dom'
 import './App.css'
 
+import StyledHeader from './components/StyledHeader'
+
 function App() {
   return (
     <>
+      <StyledHeader>
+        <Link>I'm A Link</Link>
+      </StyledHeader>
     </>
   );
 }
