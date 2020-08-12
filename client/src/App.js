edb0a8cb395472c55d65f6cc59dc64712ce76a69
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-08-12 14:42:01 -0400

    Create and basic style Footer

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ function App() {
       <StyledButton>
         ButtonFill
       </StyledButton>
+      <Footer/>
     </>
   );
 }
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import './Footer.css'
 
 export default class Footer extends Component {
     render() {
         return (
-            <div>
-                
+            <div className='footer'>
+                <p>site by <a href='/'>
+                    NocturnalMusing</a> 
+                </p>
+                <p>Fill in other information and don't forget poe</p>
             </div>
         )
     }
