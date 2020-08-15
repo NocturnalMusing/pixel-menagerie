e661bcd08745e876575a7e30e83de8cfcf8a5d6e
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-08-15 11:19:46 -0400

    Styling adoptionzone

--- client/src/screens/AdoptZone.jsx
+++ client/src/screens/AdoptZone.jsx
@@ -24,64 +24,66 @@ export default function AdoptZone(props) {
                 }
                 <Link onClick={props.handleLogout}>Logout</Link>
             </StyledHeader>
+            <div className='species-container'>
+                <header>
+                    <h1 className='adopt-zone'>Adoption Zone</h1>
+                    <h3>Click a creature to take them home</h3>
+                </header>
 
-            <header>
-                <h1 className='adopt-zone'>Adoption Zone</h1>
-                <h3>Click a creature to take them home</h3>
-            </header>
 
-            <h3 className='species-name'>Avians</h3>
-            <div>
-                {avians.map(avian =>
-                    <FontAwesomeIcon icon={`${avian.image}`} size='5x'
-                        onClick={() => handleClick(avian)
-                        } />
-                )}
-            </div>
+                <h3 className='species-name'>Avians</h3>
+                <div>
+                    {avians.map(avian =>
+                        <FontAwesomeIcon icon={`${avian.image}`} size='5x'
+                            onClick={() => handleClick(avian)
+                            } className='species-pet' />
+                    )}
+                </div>
 
-            <h3 className='species-name'>Mammals</h3>
-            <div>
-                {mammals.map(mammal =>
-                    <FontAwesomeIcon icon={`${mammal.image}`} size='5x'
-                        onClick={() => handleClick(mammal)
-                        } />
-                )}
-            </div>
+                <h3 className='species-name'>Mammals</h3>
+                <div>
+                    {mammals.map(mammal =>
+                        <FontAwesomeIcon icon={`${mammal.image}`} size='5x'
+                            onClick={() => handleClick(mammal)
+                            } className='species-pet' />
+                    )}
+                </div>
 
-            <h3 className='species-name'>Aquatic</h3>
-            <div>
-                {aquatic.map(aquatic =>
-                    <FontAwesomeIcon icon={`${aquatic.image}`} size='5x'
-                        onClick={() => handleClick(aquatic)
-                        } />
-                )}
-            </div>
+                <h3 className='species-name'>Aquatic</h3>
+                <div>
+                    {aquatic.map(aquatic =>
+                        <FontAwesomeIcon icon={`${aquatic.image}`} size='5x'
+                            onClick={() => handleClick(aquatic)
+                            } className='species-pet' />
+                    )}
+                </div>
 
-            <h3 className='species-name'>Insects</h3>
-            <div>
-                {insects.map(insect =>
-                    <FontAwesomeIcon icon={`${insect.image}`} size='5x'
-                        onClick={() => handleClick(insect)
-                        } />
-                )}
-            </div>
+                <h3 className='species-name'>Insects</h3>
+                <div>
+                    {insects.map(insect =>
+                        <FontAwesomeIcon icon={`${insect.image}`} size='5x'
+                            onClick={() => handleClick(insect)
+                            } className='species-pet' />
+                    )}
+                </div>
 
-            <h3 className='species-name'>Mythic</h3>
-            <div>
-                {mythic.map(myth =>
-                    <FontAwesomeIcon icon={`${myth.image}`} size='5x'
-                        onClick={() => handleClick(myth)
-                        } />
-                )}
-            </div>
+                <h3 className='species-name'>Mythic</h3>
+                <div>
+                    {mythic.map(myth =>
+                        <FontAwesomeIcon icon={`${myth.image}`} size='5x'
+                            onClick={() => handleClick(myth)
+                            } className='species-pet' />
+                    )}
+                </div>
 
-            <h3 className='species-name'>???</h3>
-            <div>
-                {misc.map(misc =>
-                    <FontAwesomeIcon icon={`${misc.image}`} size='5x'
-                        onClick={() => handleClick(misc)
-                        } />
-                )}
+                <h3 className='species-name'>???</h3>
+                <div>
+                    {misc.map(misc =>
+                        <FontAwesomeIcon icon={`${misc.image}`} size='5x'
+                            onClick={() => handleClick(misc)
+                            } className='species-pet' />
+                    )}
+                </div>
             </div>
 
             <Footer />
