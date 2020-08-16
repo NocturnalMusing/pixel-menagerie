ce9cfa67825f20f7326bae8498f4c204cd0166e6
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-08-16 12:20:47 -0400

    - Style Header - Adjust font colors and weights - Add font to username - Change pet image color - Finish styling

--- client/src/screens/UserPage.jsx
+++ client/src/screens/UserPage.jsx
@@ -8,26 +8,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function UserPage(props) {
 
-    const {allPets} = props
+    const { allPets } = props
 
     return (
         <>
             <StyledHeader>
-                <Link to='/'>Home</Link>
-                <Link to='/pet/adoption-zone'>Adoption Zone</Link>
-                <Link onClick={props.handleLogout}>Logout</Link>
+                <div className='user-home-adopt'>
+                    <Link className='user-home' to='/'>Home</Link>
+                    <Link to='/pet/adoption-zone'>Adoption Zone</Link>
+                </div>
+                <Link className='user-logout' onClick={props.handleLogout}>Logout</Link>
             </StyledHeader>
 
             {props.currentUser &&
                 <>
-                    <h1 className='user-welcome'>Welcome back, {props.currentUser.username}!</h1>
+                    <h1 className='user-welcome'>Welcome back, <strong>{props.currentUser.username}</strong>!</h1>
 
 
                     <div className='pets-list'>
                         {allPets && allPets.filter(pet => pet.user_id === props.currentUser.id).map(pets => (
                             <div className='pet-single'>
                                 <Link to={`/user/${props.currentUser.id}/pet/${pets.id}`}>
-                                    <FontAwesomeIcon icon={`${pets.image}`} size='5x' />
+                                    <FontAwesomeIcon className='user-pet' icon={`${pets.image}`} size='5x' />
                                 </Link>
                                 <h3 className='user-page-pet-name'>{pets.name}</h3>
                             </div>
